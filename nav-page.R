7cc26e0b91bb9bcfbea4a92ff595bd4939ac9b57
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-24 01:18:00 +0530

    Update nav-page.R

--- nav-page.R
+++ nav-page.R
@@ -1,7 +1,7 @@
 div(id = "nav_page",
   hidden(
     lapply(seq(NUM_PAGES), function(i) {
-      div( 
+      div(
         class = "page",
         id = paste0("step", i),
         source(file.path("ui", "01-navpage", sprintf("page%i.R", i)),  local = TRUE)$value
